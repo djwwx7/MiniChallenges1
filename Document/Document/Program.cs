a2e84eb016fb50c5fd457d09165b86b0ff5f0689
djwwx7 <djwwx7@mail.missouri.edu>
2018-07-06 17:21:31 -0500

    Final Solution

---
+++ Document/Document/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Document");
+
+                Console.WriteLine("Enter the name of the Document:");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter the Content for the Document:");
+
+                string content = Console.ReadLine();
+
+                string fileName = name + ".txt";
+
+                string path = Environment.CurrentDirectory + "/" + fileName;
+
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, content);
+                }
+
+                Console.WriteLine(fileName + " was successfully saved. The document contains " + content.Length + " characters");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+        }
+    }
+}
---
+++ Dog/Dog/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dog
+{
+    class Dog
+    {
+
+        public string name;
+        public string owner;
+        public int age;
+        public Gender gender;
+
+        public enum Gender
+        {
+            Male, Female
+        };
+
+        public static string getGender(Gender gender)
+        {
+            if (gender == Gender.Male)
+            {
+                return "His";
+            }
+                return "Her";
+        }
+
+        public Dog(string name_, string owner_, int age_, Gender gender_)
+        {
+            name = name_;
+            owner = owner_;
+            age = age_;
+            gender = gender_;
+        }
+
+        public void bark(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Woof! ");
+
+            }
+
+            Console.WriteLine();
+        }
+
+        public string getTag()
+        {
+            string hisHer = getGender(gender).ToString();
+
+            string heShe;
+
+            string yearYears;
+
+            if (hisHer == "His")
+            {
+                heShe = "he";
+            }
+            else
+            {
+                heShe = "she";
+            }
+
+            if(age == 1)
+            {
+                yearYears = "year";
+            }
+            else
+            {
+                yearYears = "years";
+            }
+
+            return "If lost, call " + owner + ". " + hisHer + " name is " + name + " and " + heShe + " is " + age + " " + yearYears + " old.";
+        }
+
+        static void Main(string[] args)
+        {
+
+            Dog puppy = new Dog("Orion", "Shawn", 1, Gender.Male);
+            puppy.bark(3);
+            Console.WriteLine(puppy.getTag());
+
+
+            Dog myDog = new Dog("Lileu", "Dale", 4, Gender.Female);
+            myDog.bark(1);
+            Console.WriteLine(myDog.getTag());
+        }
+    }
+}
